879e0634f2ea153fc5d8e59d2c961abf3f22ec75
sarraalqahtani <alqahtas@wfu.edu>
2022-03-14 12:11:45 -0400

    Initial commit

---
+++ src/Stack.java
@@ -0,0 +1,28 @@
+import java . util .*;
+
+class Node{
+    int data;
+    Node next=null;
+
+    public Node(int i){
+        data=i;
+    }
+    public Node(int i, Node n){
+        data=i;
+        next=n;
+    }
+}
+
+public class Stack {
+    Node head=null;
+    //create a constructor for the stack
+
+    //create a push method
+
+    //create a pop method
+
+    //create isEmpty to return true if the stack is empty or false otherwise
+
+    //create Print method to print the content of the stack without destroying it
+
+}
